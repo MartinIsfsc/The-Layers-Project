3bbe2d2a051ae838c99d4ba7156b6dababa3e69c
josvd1 <44166069+josvd1@users.noreply.github.com>
2019-01-04 19:54:28 +0100

    Add files via upload

---
+++ cartes.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  
+  <head>
+    <meta charset="utf-8">
+    <title>The Layers Project</title>
+    <link rel="icon" type="image/png" href="/image/xi.png">
+    <link rel="stylesheet" href="css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="customcss/master.css">
+    <link href="https://fonts.googleapis.com/css?family=Exo:400,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  </head>
+  
+  <body>
+
+
+    
+    <!-- L'agenda -->
+    
+
+      <a href="agenda.html" id="agenda_link"> agenda</a>
+      
+      <section id="agenda_container">
+    <div class="card-deck ">
+      <div class="card text-white " style="width: 40rem;">
+        <img class="card-img-top" src="image/unnamed.jpg" alt="Card image cap">
+        <div class="card-img-overlay btn  ">
+          <h5 class="card-title text-md-left title-spec">Conférence de laurent alexandre</h5>
+        </div>
+      </div>
+      <div class="card text-white" style="width: 40rem;">
+        <img class="card-img-top" src="image/unnamed.jpg" alt="Card image cap">
+        <div class="card-img-overlay btn">
+          <h5 class="card-title text-md-left title-spec">Conférence de laurent alexandre</h5>
+        </div>
+      </div>
+      <div class="card text-white" style="width: 40rem;">
+        <img class="card-img-top" src="image/unnamed.jpg" alt="Card image cap">
+        <div class="card-img-overlay btn">
+          <h5 class="card-title text-md-left title-spec">Conférence de laurent alexandre</h5>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+    <!-- Les News -->
+    
+    <section id="news_container">
+
+      <a href="news.html" id="news_link">news</a>
+
+       <!--<p class="news_article_titre" id="news_article_titre1">la technologie en plein coeur de la ville</p>
+
+      <img src="image/ecran.png" id="news_article1" alt="écran">
+        
+      <p class="news_article_titre" id="news_article_titre2">une ville sans embouteillages ?</p>
+
+      <img src="image/ecran.png" id="news_article2" alt="écran">
+
+      <p class="news_article_titre" id="news_article_titre3">prix de la meilleure idée de la décénie</p>
+
+      <img src="image/ecran.png" id="news_article3" alt="écran">
+      
+      <p class="news_article_titre" id="news_article_titre4">laurent alexandre, ou les révélations d'un homme déchu</p>
+    
+      <img src="image/laurent_alexandre.jpeg" id="news_article4" alt="laurent alexandre"> -->
+
+
+      <div class="card-group">
+        <div class="card text-white col-3" >
+          <img class="card-img" src="image/unnamed.jpg" alt="Card image cap">
+          <div class="card-img-overlay btn  ">
+            <h5 class="card-title text-md-left title-spec">Conférence de laurent alexandre</h5>
+          </div>
+        </div>
+        <div class="card text-white col-3">
+          <img class="card-img" src="image/unnamed.jpg" alt="Card image cap">
+          <div class="card-img-overlay btn">
+            <h5 class="card-title text-md-left title-spec">Conférence de laurent alexandre</h5>
+          </div>
+        </div>
+        <div class="card text-white col-3">
+          <img class="card-img" src="image/unnamed.jpg" alt="Card image cap">
+          <div class="card-img-overlay btn">
+            <h5 class="card-title text-md-left title-spec">Conférence de laurent alexandre</h5>
+          </div>
+        </div>
+      </div>
+      <div class="card text-white col-3">
+        <img class="card-img" src="image/unnamed.jpg" alt="Card image cap">
+        <div class="card-img-overlay btn">
+          <h5 class="card-title text-md-left title-spec">Conférence de laurent alexandre</h5>
+        </div>
+      </div>
+    </div>
+
+
+    </section>
+
+    style="width: 10rem;"
